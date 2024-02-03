aa9068a5fe9a614f61ef9d0777f07925435f3e42
Lieven Dekeyser <lievendekeyser@gmail.com>
2024-02-03 15:39:58 +0100

    Mark necessary things as public

--- Tests/OpeningHoursParserTests/OpeningHoursTests.swift
+++ Tests/OpeningHoursParserTests/OpeningHoursTests.swift
@@ -1,8 +1,12 @@
     import XCTest
-    @testable import OpeningHoursParser
+    import OpeningHoursParser
 
     final class OpeningHoursTests: XCTestCase {
-        func testExample() {
-            
+        func test24_7() throws {
+			let openingHours = OpeningHours(string: "24/7")
+			XCTAssertEqual(openingHours.ruleList.rules.count, 1)
+			
+			let rule = try XCTUnwrap(openingHours.ruleList.rules.first)
+			XCTAssert(rule.is24_7())
         }
     }
