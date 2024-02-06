125d0d887514ab8d8036568097dba21485682462
Lieven Dekeyser <lievendekeyser@gmail.com>
2024-02-06 17:01:18 +0100

    Simplify test code

--- Tests/OpeningHoursTests/OpeningHoursTests.swift
+++ Tests/OpeningHoursTests/OpeningHoursTests.swift
@@ -3,8 +3,8 @@ import XCTest
 
 final class OpeningHoursTests: XCTestCase {
 	
-	func testValidOpeningHours() throws {
-		let sampleDataURL = try XCTUnwrap(Bundle.module.url(forResource: "opening_hours_valid", withExtension: "txt"))
+	private func withSampleData(in testFileName: String, file: StaticString = #filePath, line: UInt = #line, check: (String) -> Void) throws {
+		let sampleDataURL = try XCTUnwrap(Bundle.module.url(forResource: testFileName, withExtension: "txt"), file: file, line: line)
 		
 		let sampleData = try String(contentsOf: sampleDataURL, encoding: .utf8)
 		
@@ -22,30 +22,20 @@ final class OpeningHoursTests: XCTestCase {
 			
 			let stringValue = String(components[1])
 			
+			check(stringValue)
+		}
+	}
+
+	
+	func testValidOpeningHours() throws {
+		try withSampleData(in: "opening_hours_valid") { stringValue in
 			let openingHours = OpeningHours(string: stringValue)
 			XCTAssertFalse(openingHours.hasError, "Unexpected error in \(stringValue) at position \(openingHours.errorPosition)")
 		}
 	}
 	
 	func testInvalidOpeningHours() throws {
-		let sampleDataURL = try XCTUnwrap(Bundle.module.url(forResource: "opening_hours_invalid", withExtension: "txt"))
-		
-		let sampleData = try String(contentsOf: sampleDataURL, encoding: .utf8)
-		
-		let lines = sampleData.components(separatedBy: .newlines)
-		
-		for lineIndex in lines.indices {
-			let line = lines[lineIndex]
-			
-			let components = line.split(separator: "\t", maxSplits: 1)
-			
-			
-			guard components.count == 2 else {
-				continue
-			}
-			
-			let stringValue = String(components[1])
-			
+		try withSampleData(in: "opening_hours_invalid") { stringValue in
 			let openingHours = OpeningHours(string: stringValue)
 			XCTAssertTrue(openingHours.hasError, "Expected error in \(stringValue)")
 		}
